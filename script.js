cc8b064856f5c6f97774d7f803de07100f9ffa09
nisol91 <nicolasolzi91@gmail.com>
2019-01-16 18:12:32 +0100

    4]

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 $(document).ready(function() {
 
+
+  //navbar scroll
   $(window).scroll(function() {
-  var height = $(window).scrollTop();
-  console.log(height);
-  if(height  > 100) {
-    $('.nav').addClass('nav_scroll')
-  } else if (height === 0) {
-    $('.nav').removeClass('nav_scroll')
-  }
-});
+    var height = $(window).scrollTop();
+    console.log(height);
+    if(height  > 100) {
+      $('.nav').addClass('nav_scroll')
+    } else if (height === 0) {
+      $('.nav').removeClass('nav_scroll')
+    }
+  });
+
+  //slide menu
+  $('#solutions').mouseenter(function() {
+    $('.slide').slideDown('slow');
+  });
+  $('.slide').mouseleave(function() {
+    $('.slide').slideUp('slow');
+  });
+  
 });
